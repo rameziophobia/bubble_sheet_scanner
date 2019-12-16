dc9d2880b776f8574c34a1d5b8482252dadb5f32
Ramez Noshy <RamezNoshy@gmail.com>
2019-12-16 04:56:10 +0200

    refactor get circles

--- circle_detection.py
+++ circle_detection.py
@@ -1,13 +1,11 @@
 import cv2
 import numpy as np
 import math
+
 ROW_DIFF = np.uint16(15)
-src = cv2.imread("tests/test_sample11.jpg", 1)
-# src = cv.resize(src, (800, 1200))
+src = cv2.imread("tests/test_sample6.jpg", 1)
 gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 contours, hierarchy = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-
 ret, thresh = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY_INV)
 cv2.namedWindow("gray", cv2.WINDOW_NORMAL)
 cv2.imshow("gray", thresh)
@@ -19,45 +17,35 @@ eroded_thresh = eroded_thresh[height // 4:height, width // 2:width]
 img_display = src[height // 4:height, width // 2:width]
 
 black_circles = cv2.HoughCircles(eroded_thresh, cv2.HOUGH_GRADIENT, 1, 10,
-                           param1=10, param2=11,
-                           minRadius=1, maxRadius=15)
+                                 param1=10, param2=11,
+                                 minRadius=1, maxRadius=15)
 
 thresh = thresh[height // 4:height, width // 2:width]
 mixed_circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 1, 10,
-                            param1=10, param2=15,
-                            minRadius=9, maxRadius=15)
+                                 param1=10, param2=15,
+                                 minRadius=9, maxRadius=15)
 
 
-def loop_circles(circles):
+# def get_circle_centers(circles):
+#     if circles is not None:
+#         return [(center[0], center[1]) for center in circles[0]]
+
+
+def get_centers_x_sorted(circles):
     if circles is not None:
-        circles = np.uint16(np.around(circles))
-        # print(type(circles))
-        for x, y, radius in circles[0, :]:
-            all_centers_XandY.append((x, y))
-            # print(f"x {x}, y {y}")
-            center = (x, y)
-            # circle center
-            cv2.circle(img_display, center, 1, (0, 100, 100), 3)
-            # circle outline
-            cv2.circle(img_display, center, radius, (255, 0, 255), 3)
+        return sorted([center[0] for center in circles])
 
 
-all_centers_XandY = []
+def get_centers_y_sorted(circles):
+    if circles is not None:
+        return sorted([center[1] for center in circles])
 
-loop_circles(black_circles)
-loop_circles(mixed_circles)
 
-cv2.namedWindow("detected circles", cv2.WINDOW_NORMAL)
-cv2.imshow("detected circles", src)
-cv2.imwrite("detected circles.jpg", img_display)
-cv2.imwrite("thresh.jpg", thresh)
-cv2.waitKey(0)
+all_circles = []
+all_circles.extend(black_circles[0])
+all_circles.extend(mixed_circles[0])
 
-# todo
-# sort all circles 7asab el y
-
-centers_sorted_by_y = sorted(all_centers_XandY, key=lambda circle: circle[1])
-centers_sorted_by_y = [(center[1]) for center in centers_sorted_by_y]
+centers_sorted_by_y = get_centers_y_sorted(all_circles)
 questions_y_list = [centers_sorted_by_y[0]]
 last_y = centers_sorted_by_y[0]
 
@@ -70,37 +58,22 @@ for y in centers_sorted_by_y:
         last_y = y
 # last y feha el y's bta3et kol row we sorted kman shoft ba2a el 7lawa
 
-
-# todo
-# 1. sort list of all detected circles based on x
-# 2. get max_x, min_x
-# 3. define span as (max_x - min_x) / 5
-
-centers_sorted_by_x = sorted(all_centers_XandY, key=lambda circle : circle[0])
-max_x = centers_sorted_by_x[-1][0]
-min_x = centers_sorted_by_x[0][0]
+centers_sorted_by_x = get_centers_x_sorted(all_circles)
+max_x = centers_sorted_by_x[-1]
+min_x = centers_sorted_by_x[0]
 span = (max_x - min_x) / 5
-# loop over black_circles
-#   ans_num = (circle.x - min.x) / span
-#
-answers = []
-# print(type(black_circles))
-black_circles = black_circles.tolist()[0]
-# print(type(black_circles))
 
+answers = []
+black_circles = black_circles.tolist()[0]
 black_circles.sort(key=lambda circle: circle[1])
 c = 0
-# print(len(black_circles))
 for y in questions_y_list:
     ans_num = (black_circles[c][0] - min_x) / span
-    # print(f"min: {min_x}, max: {max_x}, span: {span}, reg: {max_x - min_x}")
-    # print(f"x{c}: {black_circles[c][0]}, y: {black_circles[c][1]}")
     if abs(y - black_circles[c][1]) < 5:
         answers.append(int(1 + math.floor(ans_num)))
         c = c + 1
     else:
         answers.append("Unanswered")
-
 
 print(answers)
 
